2a3e84c8eb200de57dc7d1ea996c1f2344cca30b
DataNick <DataNick@users.noreply.github.com>
2016-07-21 22:34:07 -0400

    Generated scaffold for portfolio

---
+++ db/migrate/20160722023118_create_portfolios.rb
@@ -0,0 +1,10 @@
+class CreatePortfolios < ActiveRecord::Migration
+  def change
+    create_table :portfolios do |t|
+      t.string :name
+      t.float :amount
+
+      t.timestamps null: false
+    end
+  end
+end
