98610fbe66e7dd10aad6372f2020a1893e2b1300
DataNick <DataNick@users.noreply.github.com>
2016-07-29 15:53:03 -0400

    create method

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,7 +1,15 @@
 class StocksController < ApplicationController
-  before_action :set_stock, only: [:create, :update, :destroy, :show]
+  # before_action :set_stock, only: [:create, :update, :destroy, :show]
 
   def create
+    portfolio = Portfolio.find(params[:id])
+    stock = Stock.new(stock_params.merge! portfolio_id: portfolio.id)
+
+    if stock.save
+      redirect_to portfolio, notice: "Stock was added to portfolio."
+    else
+      redirect_to portfolio, alert: "Something went wrong."
+    end
   end
 
   def update
@@ -12,9 +20,9 @@ class StocksController < ApplicationController
 
   private
 
-  def set_stock
-    @stock = Stock.find(params[:id])
-  end
+  # def set_stock
+  #   @stock = Stock.find(params[:id])
+  # end
 
   def stock_params
     params.require(:stock).permit(:symbol, :name, :stock_exchange, :last_price)
