605e2e045426c8687ac617b186d836648cb1b873
DataNick <DataNick@users.noreply.github.com>
2016-07-29 14:52:27 -0400

    Modified model to check data

--- lib/stock_fetcher.rb
+++ lib/stock_fetcher.rb
@@ -3,10 +3,6 @@ require 'json'
 class StockFetcher
   attr_reader :stock
 
-  # BASE_URL = 'https://query.yahooapis.com/v1/public/yql?q='
-  # QUERY = URI.encode("select * from yahoo.finance.quotes where symbol in (#{@stock})")
-  # FORMAT = "&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
-
   def initialize(stock)
     @stock = stock
   end
@@ -16,10 +12,13 @@ class StockFetcher
     url = 'https://query.yahooapis.com/v1/public/yql?q='
     url += URI.encode("select * from yahoo.finance.quotes where symbol in (#{ticker})")
     url += "&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
-    data = open(url, {:read_timeout => 3}).read
+    data = open(url, {:read_timeout => 5}).read
     stock_hash = JSON.parse(data)
   end
 end
 
-stock = StockFetcher.new('yhoo')
-p stock.fetch['query']['results']['quote']['LastTradePriceOnly'].to_f
+stock = StockFetcher.new('googl')
+p stock.fetch['query']['results']['quote']['LastTradeWithTime'].split.first
+# p stock.fetch['query']['results']['quote']["LastTradeWithTime"]
+# StockFetcher.new(Stock.new('GOOGL'))
+# p StockFetcher.new
