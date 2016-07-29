a1fd3e6bfeea7852f9db814642347ee66cc02f8f
DataNick <DataNick@users.noreply.github.com>
2016-07-29 14:49:56 -0400

    Included accepts nested attributes for transactions and stocks

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,4 +2,10 @@ class Portfolio < ActiveRecord::Base
   belongs_to :user
   has_many :transactions
   has_many :stocks, through: :transactions
+
+  accepts_nested_attributes_for :transactions
+  accepts_nested_attributes_for :stocks
+
+  # Managing a person's portfolio through transactions
+
 end
