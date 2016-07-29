43b30670700a743da876e8dccb791ee6dfb997d3
DataNick <DataNick@users.noreply.github.com>
2016-07-29 15:52:34 -0400

    Added new, index, create, update and transaction_params

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,13 +1,43 @@
 class TransactionsController < ApplicationController
   def new
+    @transaction = Transaction.new
+  end
+
+  def index
+    @transactions = Transaction.all
   end
 
   def create
+    @transaction = Transaction.new(transaction_params)
+
+    respond_to do |format|
+      if @transaction.save
+        format.html { redirect_to @transaction, notice: 'Transaction was successfully created.' }
+        format.json { render :show, status: :created, location: @transaction }
+      else
+        format.html { render :new }
+        format.json { render json: @transaction.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
+    respond_to do |format|
+      if @transaction.update(transaction_params)
+        format.html { redirect_to @transaction, notice: 'Transaction was successfully updated.' }
+        format.json { render :show, status: :ok, location: @transaction }
+      else
+        format.html { render :edit }
+        format.json { render json: @transaction.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def show
+  end
+
+  private
+  def transaction_params
+    params.require(:transaction).permit(:type, :num_of_shares, :price, :portfolio_id, :stock_id)
   end
 end
