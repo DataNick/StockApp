ea02b04760655aab74b71c9c0b9f96ec2d27c0f8
DataNick <DataNick@users.noreply.github.com>
2016-07-29 22:50:14 -0400

    Modified associations

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,8 +3,8 @@ class Portfolio < ActiveRecord::Base
   has_many :transactions
   has_many :stocks, through: :transactions
 
-  accepts_nested_attributes_for :transactions
-  accepts_nested_attributes_for :stocks
+  # accepts_nested_attributes_for :transactions
+  # accepts_nested_attributes_for :stocks
 
   # Managing a person's portfolio through transactions
 
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,5 +2,5 @@ require_relative '../../lib/stock_fetcher.rb'
 
 class Stock < ActiveRecord::Base
   has_many :transactions
-  has_many :portfolios, through: :transactions
+  # has_many :portfolios, through: :transactions
 end
