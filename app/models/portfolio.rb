7849bb055f61a0c8a58ff5ed1b527c0afa1f46bb
DataNick <DataNick@users.noreply.github.com>
2016-07-30 22:39:40 -0400

    Cleaned up unnecessary comments

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,10 +2,4 @@ class Portfolio < ActiveRecord::Base
   belongs_to :user
   has_many :transactions
   has_many :stocks, through: :transactions
-
-  # accepts_nested_attributes_for :transactions
-  # accepts_nested_attributes_for :stocks
-
-  # Managing a person's portfolio through transactions
-
 end
