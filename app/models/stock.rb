71d445f95ddc184ed129b53349677a606f836b54
DataNick <DataNick@users.noreply.github.com>
2016-08-03 15:20:16 -0400

    Created constant for company list

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,5 +2,17 @@ require_relative '../../lib/stock_fetcher.rb'
 
 class Stock < ActiveRecord::Base
   has_many :transactions
-  has_many :portfolios, through: :transactions
+  # has_many :portfolios, through: :transactions
+
+  COMPANIES = [
+    "TSLA",
+    "TWTR",
+    "AAPL",
+    "FB",
+    "GOOGL",
+    "ATVI",
+    "AMZN",
+    "LNKD",
+    "MFST"
+  ]
 end
