2a43f7c0935956c8a1c46718e13e5de892120f5b
DataNick <DataNick@users.noreply.github.com>
2016-08-10 16:35:05 -0400

    Ran migration on transactions table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160730211047) do
+ActiveRecord::Schema.define(version: 20160809022238) do
 
   create_table "portfolios", force: :cascade do |t|
     t.string   "name"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 20160730211047) do
     t.integer  "portfolio_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-    t.string   "stock_id"
+    t.integer  "stock_id"
     t.datetime "buy_date"
     t.decimal  "buy_price"
     t.decimal  "sell_price"
