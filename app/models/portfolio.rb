c09bfa8d7973a0445381268b10c80e3d972250ff
DataNick <DataNick@users.noreply.github.com>
2016-08-14 04:36:51 -0400

    Created total_value method

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,4 +2,9 @@ class Portfolio < ActiveRecord::Base
   belongs_to :user
   has_many :transactions
   has_many :stocks, through: :transactions
+
+  def total_value
+    stocks_value = self.stocks.inject {|sum, stock| sum + stock.last_price}
+    amount + stock_value
+  end
 end
