8367de9f8da35d7c7ee45dea9d593caaf7c14690
DataNick <DataNick@users.noreply.github.com>
2016-08-22 12:32:23 -0400

    Removed require_relative call to stock_fetcher model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,3 @@
-require_relative '../../lib/stock_fetcher.rb'
-require 'open-uri'
-require 'json'
 class Transaction < ActiveRecord::Base
   belongs_to :portfolio
   belongs_to :stock
@@ -8,13 +5,15 @@ class Transaction < ActiveRecord::Base
   # def buy(ticker, quantity)
   #   ticker.upcase!
   # end
-  def get_price(sym)
-    sym = sym.inspect.upcase
-    url = 'https://query.yahooapis.com/v1/public/yql?q='
-    url += URI.encode("select * from yahoo.finance.quotes where symbol in (#{sym})")
-    url += "&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
-    data = open(url, {:read_timeout => 5}).read
-    stock_hash = JSON.parse(data)
-  end
+
+
+  # def get_price(sym)
+  #   sym = sym.inspect.upcase
+  #   url = 'https://query.yahooapis.com/v1/public/yql?q='
+  #   url += URI.encode("select * from yahoo.finance.quotes where symbol in (#{sym})")
+  #   url += "&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
+  #   data = open(url, {:read_timeout => 5}).read
+  #   stock_hash = JSON.parse(data)
+  # end
 
 end
