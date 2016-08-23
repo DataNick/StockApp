55814d730553b34084446fef7cac483ffb27044d
DataNick <DataNick@users.noreply.github.com>
2016-08-22 20:43:13 -0400

    Added extra time to timeout error

--- lib/stock_fetcher.rb
+++ lib/stock_fetcher.rb
@@ -26,7 +26,7 @@ class StockFetcher
     url = 'https://query.yahooapis.com/v1/public/yql?q='
     url += URI.encode("select * from yahoo.finance.quotes where symbol in (#{tickers})")
     url += "&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
-    data = open(url, {:read_timeout => 5}).read
+    data = open(url, {:read_timeout => 7}).read
     parse_data(data) # returns an array of hashes
   end
 
