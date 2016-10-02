103ce3651230e1f84b2e23597e577d841db1d825
DataNick <DataNick@users.noreply.github.com>
2016-10-02 00:55:11 -0400

    Calculate weighted average for trade

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,4 +3,26 @@ class Transaction < ActiveRecord::Base
   belongs_to :stock
   validates :stock_id, presence: true
   validates :num_of_shares, presence: true
+
+  TYPES = ["buy", "sell"]
+
+  def sell(quantity)
+    self.num_of_shares =- quantity
+  end
+
+  def investment
+    if buy_price
+      num_of_shares * buy_price
+    end
+  end
+
+  def gain_loss
+    ( ( ( sell_price - buy_price ) / buy_price ) * 100 ).round(2)
+  end
+
+  def weighted_average
+     #(first gain * (investment / total investment)) + (second gain *(investment / total investment)) + (third gain * (investment / total investment))
+     (gain_loss * (investment / @portfolio.investment))
+  end
+
 end
