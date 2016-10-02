8695b6016a919ad9e276df648f33ddeb37914d79
DataNick <DataNick@users.noreply.github.com>
2016-10-02 00:54:13 -0400

    Calculate weighted average

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -5,13 +5,43 @@ class Portfolio < ActiveRecord::Base
 
   validates :name,  uniqueness: true, length: { minimum: 5 }
 
-  # def total_value
-  #   stocks_value = self.stocks.inject {|sum, stock| sum + stock.last_price}
-  #   amount + stock_value
-  # end
 
   def portfolio_value
-    # self.transactions.collect {|ti| ti.num_of_shares * ti.buy_price }.sum
     transactions.map{|trans| trans.stock.last_price * trans.num_of_shares }.inject(:+)
   end
+
+  # def initial_cost
+  #   transactions.collect{|trade| trade.buy_price ? trade.buy_price.to_f * trade.num_of_shares : 0 }.sum
+  # end
+
+  def gain_loss
+    transaction.collect{|trade| (trade.sell_price - trade.buy_price) / trade.buy_price }
+    #-- or -- (tranasaction.sell_price - transaction.buy_price) / transaction.buy_price
+  end
+
+  def total_investment
+    total = 0.0
+    transactions.each{ |trade| total += trade.investment }
+    total
+  end
+
+  def weighted_average
+    total = 0.0
+    transactions.each{ |trade| total += trade.weighted_average }
+    total
+  end
+
+  #Average gain/loss
+  #annualized returns?
+  #Investment = number of shares * buy price
+  #@portfolio.transactions.average(:gain/loss) => average
+  #want to get weighted average - different portion of investment in some versus others
+    #(first gain * (investment / total investment)) + (second gain *(investment / total investment)) + (third gain * (investment / total investment))
+
+#     Portfolio weight - % composition of a particular holding in a portfolio.
+# Different approaches to calculating weight.
+# 1. Divide the dollar value of a security by the total dollar value of the portfolio
+# 2. Divide the number of units of a given security by the total number of shares held in the portfolio
+# 3. Market value of stock position = multiply the share price by the number of shares outstanding
+
 end
