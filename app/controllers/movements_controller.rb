f238ef54ed540d86a757b54adc41ce8e2cc5222d
DataNick <DataNick@users.noreply.github.com>
2016-10-07 00:59:03 -0400

    Add movements controller

---
+++ app/controllers/movements_controller.rb
@@ -0,0 +1,27 @@
+class MovementsController < ApplicationController
+  before_action :set_transaction
+
+  def index
+  end
+
+  def create
+    @movement = Movement.new(movement_params)
+
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  private
+
+    def set_transaction
+      @transaction = Transaction.find(params[:portfolio][:transaction_id])
+    end
+
+    def movement_params
+      params.require(:movement).permit(:price, :date, :transaction_id, :action)
+    end
+end
